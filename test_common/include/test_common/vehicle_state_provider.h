0c1b6484a77601beee9712a31e7ffb5e0efb1705
mujianhua <jianhua_mu@163.com>
2022-07-14 21:34:02 +0800

    vehicle_state_provider library

---
+++ test_common/include/test_common/vehicle_state_provider.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "test_common/ChassisData.h"
+namespace mujianhua {
+namespace control {
+
+class VehicleStateProvider {
+  public:
+    VehicleStateProvider() = default;
+
+    bool Update(const test_common::ChassisData *chassis_data);
+
+    double x() const;
+
+    double y() const;
+
+    double theta() const;
+
+    double roll() const;
+
+    double pitch() const;
+
+    double vx() const;
+
+    double vy() const;
+
+    double yawrate() const;
+
+    double rollrate() const;
+
+  private:
+    const test_common::ChassisData *data_;
+};
+
+} // namespace control
+} // namespace mujianhua
--- test_planning/src/test_planning_node.cpp
+++ test_planning/src/test_planning_node.cpp
@@ -1,13 +1,17 @@
+#include <ros/ros.h>
+#include "gtest/gtest.h"
 #include "test_common/PathPoint.h"
 #include "test_common/TrajectoryPoint.h"
-#include "gtest/gtest.h"
-#include <ros/ros.h>
 
 class PlanningNode {
   public:
-    PlanningNode(ros::NodeHandle nh) : nh_(nh) {
+    explicit PlanningNode(const ros::NodeHandle &nh) : nh_(nh) {
         planning_pub_ =
             nh_.advertise<test_common::TrajectoryPoint>("planning", 1);
+        test_common::TrajectoryPoint trajectory_point;
+        trajectory_point.path_point.x = 0.1;
+        planning_pub_.publish(trajectory_point);
+        ROS_INFO("%f", trajectory_point.path_point.x);
     }
 
   private:
@@ -18,6 +22,10 @@ class PlanningNode {
 int main(int argc, char **argv) {
     ros::init(argc, argv, "ros_planning");
     ros::NodeHandle nh;
+
+    PlanningNode node(nh);
+
+    ros::spin();
 
     return 0;
 }
