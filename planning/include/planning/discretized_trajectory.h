975e811e66763ef11fa25eccc536f09cc5d24923
mujianhua <jianhua_mu@163.com>
2022-08-21 19:07:11 +0800

    ...

--- planning/include/planning/discretized_trajectory.h
+++ planning/include/planning/discretized_trajectory.h
@@ -1,13 +1,3 @@
-/***********************************************************************************
- *  C++ Source Codes for "Autonomous Driving on Curvy Roads without Reliance on
- *  Frenet Frame: A Cartesian-based Trajectory Planning Method".
- ***********************************************************************************
- *  Copyright (C) 2022 Bai Li
- *  Users are suggested to cite the following article when they use the source
- *codes. Bai Li et al., "Autonomous Driving on Curvy Roads without Reliance on
- *  Frenet Frame: A Cartesian-based Trajectory Planning Method",
- *  IEEE Transactions on Intelligent Transportation Systems, 2022.
- ***********************************************************************************/
 
 #pragma once
 
---
+++ planning/src/lattice/trajectory1d_generetor.cpp
@@ -0,0 +1,7 @@
+#include "lattice/trajectory1d_generetor.h"
+
+namespace planning {
+
+Trajectory1dGenerator::Trajectory1dGenerator() {}
+
+}  // namespace planning
