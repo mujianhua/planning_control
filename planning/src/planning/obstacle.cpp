f2d3a9344ca7c063eb6fe8405b31777e15391be7
mujianhua <jianhua_mu@163.com>
2022-08-25 14:38:12 +0800

    obstacle

---
+++ planning/src/planning/obstacle.cpp
@@ -0,0 +1,12 @@
+
+#include "planning/obstacle.h"
+
+namespace planning {
+
+Obstacle::Obstacle(const std::string &id, const StaticObstacle &obs)
+    : id_(id), is_static_(true), static_obs_(obs) {}
+
+Obstacle::Obstacle(const std::string &id, const DynamicObstacle &obs)
+    : id_(id), is_static_(false), dynamic_obs_(obs) {}
+
+}  // namespace planning
